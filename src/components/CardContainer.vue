f66194333c58bea7e6067c755b65291e445b3400
mando <nsa06031@gmail.com>
2020-12-27 14:06:25 +0900

    feat: Infinite Scroll 컴포넌트 구현

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="card-container">
     <Card v-for="card in cards" v-bind="card" :key="card.id" />
+    <InfiniteScroll @load="fetchCards" />
   </div>
 </template>
 
 <script>
 import Card from '@/components/Card.vue';
+import InfiniteScroll from '@/components/InfiniteScroll.vue';
 import { mapActions, mapState } from 'vuex';
 import { FETCH_CARDS } from '../store/types';
 
 export default {
   name: 'CardContainer',
   components: {
-    Card
+    Card,
+    InfiniteScroll
   },
   computed: {
     ...mapState(['cards'])
@@ -24,6 +27,8 @@ export default {
   },
   created() {
     this.fetchCards();
+    // this.debouncedFetchCards = debounce(this.fetchCards, 500);
+    // this.debouncedFetchCards();
   }
 };
 </script>
